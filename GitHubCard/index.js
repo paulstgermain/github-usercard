c6cf9b180957f591aec08991536f98eadd652c81
paulstgermain <paulg.stgermain@gmail.com>
2021-01-14 02:16:43 -0500

    initial commit | met all mvp goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const cardsDiv = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/paulstgermain')
+.then(result => {
+    // console.log(result.data);
+    // console.log(cardMaker(result.data));
+    cardsDiv.append(cardMaker(result.data));
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +38,21 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['bigknell', 'dustinmyers'];
+
+axios.get('https://api.github.com/users/paulstgermain/followers')
+.then(result => {
+    Array.from(result.data).forEach(item => {
+      followersArray.unshift(item.login);
+    })
+  }).then(result => {
+    followersArray.forEach(item => {
+      axios.get(`https://api.github.com/users/${item}`)
+      .then(result => {
+        cardsDiv.append(cardMaker(result.data));
+      })
+    })
+  })
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +73,65 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+function cardMaker(data){
+  // CREATE ALL CARD ELEMENTS
+  const card = document.createElement('div');
+  const profileImage = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  // PUT THE PIECES TOGETHER WITH APPEND
+  card.append(profileImage);
+  card.append(cardInfo);
+  cardInfo.append(name);
+  cardInfo.append(username);
+  cardInfo.append(location);
+  cardInfo.append(profileLink);
+  cardInfo.append(followers);
+  cardInfo.append(following);
+  cardInfo.append(bio);
+  // ADD NECESSARY CLASSES
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+  // PULL THE USER INFO FROM API AND PLACE IN CARD
+  profileImage.setAttribute('src', data.avatar_url);
+  name.innerText = data.name;
+  username.innerText = data.login;
+  location.innerText = `Location: ${data.location}`;
+  profileLink.setAttribute('href', data.html_url);
+  profileLink.innerText = 'Profile';
+  followers.innerText = `Followers: ${data.followers}`;
+  following.innerText = `Following: ${data.following}`;
+  bio.innerText = `Bio: ${data.bio}`;
+
+  return card;
+
+    // cardDiv.innerHTML = `
+    // <div class="card">
+    //   <img src="${data.avatar_url}" />
+    //   <div class="card-info">
+    //     <h3 class="name">${data.name}</h3>
+    //     <p class="username">${data.login}</p>
+    //     <p>Location: ${data.location}</p>
+    //     <p>Profile:
+    //       <a href="${data.html_url}">${data.html_url}</a>
+    //     </p>
+    //     <p>Followers: ${data.followers}</p>
+    //     <p>Following: ${data.following}</p>
+    //     <p>Bio: ${data.bio}</p>
+    //   </div>
+    // </div>
+    // `;
+}
 
 /*
   List of LS Instructors Github username's:
